c7f630bbadd95b1dd1d7346fc5137e7da4012713
balli <g00380316@atu.ie>
2023-07-18 21:54:31 +0100

    Test method and method for add complete

--- src/main/java/ie/atu/Calculator.java
+++ src/main/java/ie/atu/Calculator.java
@@ -1,4 +1,15 @@
 package ie.atu;
 
+
+
 public class Calculator {
+    private int firstNumber,secondNumber,total;
+        public Calculator(){firstNumber = 0; secondNumber = 0; total= 0;}
+
+        public int add(int firstNumber, int secondNumber){
+            total = firstNumber + secondNumber;
+            System.out.println("The total is "+ total);
+
+            return total;
+        }
 }
